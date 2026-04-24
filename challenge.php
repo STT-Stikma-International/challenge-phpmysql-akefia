70ccf7d493a0653b54e4ca2fb547707f754d60e6
unknown <malangparanoid404@gmail.com>
2026-04-24 15:23:10 +0700

    24112673

--- challenge.php
+++ challenge.php
@@ -1,76 +1,23 @@
 <?php
-include 'koneksi.php';
+// include 'koneksi.php';
 
-$nama = $_POST['nama'];
-$nim = $_POST['nim'];
-$kelas = $_POST['kelas'];
-$email = $_POST['email'];
-
-
-//TODO: insert nama, nim, kelas, email
-
-// ---
-if ($conn->query($sql) === TRUE) {
-    echo "Data berhasil disimpan";
-} else {
-    echo "Error: " . $conn->error;
-}
-
-$conn->close();
+// $nama = $_POST['nama'];
+// $nim = $_POST['nim'];
+// $kelas = $_POST['kelas'];
+// $email = $_POST['email'];
 
 
+// //TODO: insert nama, nim, kelas, email
+// $sql = "INSERT INTO mahasiswa (nama, nim , kelas, email)
+// VALUES ('$nama', '$nim ', '$kelas', '$email')";
+// // ---
+// if ($conn->query($sql) === TRUE) {
+//     echo "Data berhasil disimpan";
+// } else {
+//     echo "Error: " . $conn->error;
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//TODO: SELECT data mahasiswa
-
-// ---
-if ($result->num_rows > 0) {
-
-    echo "<table border='1'>";
-    echo "<tr>
-            <th>ID</th>
-            <th>Nama</th>
-            <th>NIM</th>
-            <th>Kelas</th>
-            <th>Email</th>
-        </tr>";
-
-    while($row = $result->fetch_assoc()) {
-        echo "<tr>
-                <td>".$row['id']."</td>
-                <td>".$row['nama']."</td>
-                <td>".$row['nim']."</td>
-                <td>".$row['kelas']."</td>
-                <td>".$row['email']."</td>
-            </tr>";
-    }
-
-    echo "</table>";
-
-} else {
-    echo "Data tidak ditemukan";
-}
-$conn->close();
+// $conn->close();
 
 
 
@@ -94,9 +41,65 @@ $conn->close();
 
 
 
-//TODO: UPDATE DATA MAHASISWA
 
-//---
+// //TODO: SELECT data mahasiswa
+// $sql = "SELECT nama, nim, kelas, email, FROM mahasiswa";
+// $result = $conn-> query($sql);
+// // ---
+// if ($result->num_rows > 0) {
+
+//     echo "<table border='1'>";
+//     echo "<tr>
+//             <th>ID</th>
+//             <th>Nama</th>
+//             <th>NIM</th>
+//             <th>Kelas</th>
+//             <th>Email</th>
+//         </tr>";
+
+//     while($row = $result->fetch_assoc()) {
+//         echo "<tr>
+//                 <td>".$row['id']."</td>
+//                 <td>".$row['nama']."</td>
+//                 <td>".$row['nim']."</td>
+//                 <td>".$row['kelas']."</td>
+//                 <td>".$row['email']."</td>
+//             </tr>";
+//     }
+
+//     echo "</table>";
+
+// } else {
+//     echo "Data tidak ditemukan";
+// }
+// $conn->close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //TODO: UPDATE DATA MAHASISWA
+$sql = "UPDATE mahasiswa"
+SET email='bkonci@gmail.com' WHERE id=6";
+// //---
 if ($conn->query($sql) === TRUE) {
     echo "Data berhasil diupdate";
 } else {
